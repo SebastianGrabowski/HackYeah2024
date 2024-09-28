2d177791108c9835f526b20b9374c94a2b138061
sgrabowski <sebastian.s.grabowski@gmail.com>
2024-09-28 19:25:07 +0200

    t2

--- HackYeah2024/Assets/TestMic/TestMic.cs
+++ HackYeah2024/Assets/TestMic/TestMic.cs
@@ -5,77 +5,27 @@ using UnityEngine.Windows.Speech;
 
 public class InputMic : MonoBehaviour
 {
-    protected DictationRecognizer _Recognizer;
+    protected KeywordRecognizer _Recognizer;
+    public string[] keywords = new string[] {"up", "down", "left", "right"};
+    public ConfidenceLevel confidence = ConfidenceLevel.Medium;
 
-    void Start()
+    private void Start()
     {
-        StartDictationEngine();
+        _Recognizer = new KeywordRecognizer(keywords, confidence);
+        _Recognizer.OnPhraseRecognized += Recognizer_OnPhraseRecognized;
+        _Recognizer.Start();
     }
-
-    private void DictationRecognizer_OnDictationHypothesis(string text)
+    private void Recognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
     {
-        Debug.Log("Dictation hypothesis: " + text);
-    }
-
-    private void DictationRecognizer_OnDictationResult(string text, ConfidenceLevel confidence)
-    {
-        Debug.Log("Dictation result: " + text);
-    }
-
-    private void DictationRecognizer_OnDictationError(string error, int hresult)
-    {
-        Debug.Log("Dictation error: " + error);
-    }
-
-
-    private void DictationRecognizer_OnDictationComplete(DictationCompletionCause completionCause)
-    {
-        switch (completionCause)
-        {
-            case DictationCompletionCause.TimeoutExceeded:
-            case DictationCompletionCause.PauseLimitExceeded:
-            case DictationCompletionCause.Canceled:
-            case DictationCompletionCause.Complete:
-                CloseDictationEngine();
-                StartDictationEngine();
-                break;
-            case DictationCompletionCause.UnknownError:
-            case DictationCompletionCause.AudioQualityFailure:
-            case DictationCompletionCause.MicrophoneUnavailable:
-            case DictationCompletionCause.NetworkFailure:
-                CloseDictationEngine();
-                break;
-        }
+        Debug.Log(args.text);
     }
 
     private void OnApplicationQuit()
     {
-        CloseDictationEngine();
-    }
-
-    private void StartDictationEngine()
-    {
-        _Recognizer = new DictationRecognizer();
-        _Recognizer.DictationHypothesis += DictationRecognizer_OnDictationHypothesis;
-        _Recognizer.DictationResult += DictationRecognizer_OnDictationResult;
-        _Recognizer.DictationComplete += DictationRecognizer_OnDictationComplete;
-        _Recognizer.DictationError += DictationRecognizer_OnDictationError;
-        _Recognizer.Start();
-    }
-
-    private void CloseDictationEngine()
-    {
-        if (_Recognizer != null)
+        if (_Recognizer != null && _Recognizer.IsRunning)
         {
-            _Recognizer.DictationHypothesis -= DictationRecognizer_OnDictationHypothesis;
-            _Recognizer.DictationComplete -= DictationRecognizer_OnDictationComplete;
-            _Recognizer.DictationResult -= DictationRecognizer_OnDictationResult;
-            _Recognizer.DictationError -= DictationRecognizer_OnDictationError;
-            if (_Recognizer.Status == SpeechSystemStatus.Running)
-            {
-                _Recognizer.Stop();
-            }
-            _Recognizer.Dispose();
+            _Recognizer.OnPhraseRecognized -= Recognizer_OnPhraseRecognized;
+            _Recognizer.Stop();
         }
     }
 }
