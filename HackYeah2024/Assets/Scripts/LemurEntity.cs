9ece9fe53007c6792d089e228c7dd4e594bdab5b
Łukasz Surma <lukaszsurma73@gmail.com>
2024-09-28 22:59:05 +0200

    entites setup

--- HackYeah2024/Assets/Scripts/LemurEntity.cs
+++ HackYeah2024/Assets/Scripts/LemurEntity.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class LemurEntity : MonoBehaviour
 {
+    [SerializeField] private float _destroyForce;
     public Collider Collider;
     public PlayerController PlayerController;
     public Vector3 Target;
     public bool IsInTeam;
 
+    private Rigidbody _rigidbody;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
     public void SetInTeam()
     {
         IsInTeam = true;
         Collider.isTrigger = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Obstacle"))
+        {
+            // destroy effect to setup later
+
+            // _rigidbody.isKinematic = false;
+            // _rigidbody.AddForce(transform.forward * _destroyForce, ForceMode.Impulse);
+
+            if(PlayerController == null)
+                PlayerController = FindObjectOfType<PlayerController>();
+            
+            PlayerController.RemoveLemur(this);
+        }
+
+        if(!IsInTeam && other.TryGetComponent(out PlayerController playerController))
+        {
+            playerController.AddLemur(this);
+        }
     }
 
     // Update is called once per frame
@@ -21,7 +50,7 @@ public class LemurEntity : MonoBehaviour
         if(!IsInTeam)
             return;
 
-        var dir = (PlayerController.TargetPos + Target)-transform.position;
-        transform.position += dir.normalized * Time.deltaTime * 4.0f;
+        var dir = (PlayerController.TargetPos + Target) - transform.position;
+        _rigidbody.velocity += dir.normalized * Time.deltaTime * 4.0f * Mathf.Min(1, dir.magnitude);
     }
 }
