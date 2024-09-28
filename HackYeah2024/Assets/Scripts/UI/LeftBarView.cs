8785bff8b8193aa24143bb952714b22878a5ac12
sgrabowski <sebastian.s.grabowski@gmail.com>
2024-09-29 00:31:17 +0200

    asdasd

--- HackYeah2024/Assets/Scripts/UI/LeftBarView.cs
+++ HackYeah2024/Assets/Scripts/UI/LeftBarView.cs
@@ -4,10 +4,12 @@ namespace Game.UI
     using System.Collections;
     using System.Collections.Generic;
     using UnityEngine;
+    using TMPro;
 
     public class LeftBarView : MonoBehaviour
     {
         [SerializeField]private GameObject _view;
+        [SerializeField]private TMP_Text _valueLabel;
 
         public void Show()
         {
@@ -17,6 +19,14 @@ namespace Game.UI
         public void Hide()
         {
             _view.SetActive(false);
+        }
+
+        private IEnumerator DisplayCommand(string value)
+        {
+            _valueLabel.transform.parent.gameObject.SetActive(true);
+            _valueLabel.text = value;
+            yield return new WaitForSeconds(2.0f);
+            _valueLabel.transform.parent.gameObject.SetActive(false);
         }
     }
 }
