0842f1fdcb3997b49dbb84db7a1e5e45f148705c
Łukasz Surma <lukaszsurma73@gmail.com>
2024-09-28 21:35:02 +0200

    entity collection

--- HackYeah2024/Assets/Scripts/PlayerController.cs
+++ HackYeah2024/Assets/Scripts/PlayerController.cs
@@ -20,11 +20,27 @@ public struct MoveData
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float _speed;
+    [SerializeField] private Transform _lemursParent;
+    [SerializeField] private Vector3[] _formationsTemp;
     [SerializeField] private MoveData[] _moveData;
+
+    private List<LemurEntity> _lemurs = new List<LemurEntity>();
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.TryGetComponent(out LemurEntity lemurEntity))
+        {
+            AddLemur(lemurEntity);
+        }
+    }
 
     void Update()
     {
         transform.Translate(Vector3.forward * _speed * Time.deltaTime);
+
+        //Debug Movement
+        if(Input.GetKeyDown(KeyCode.A)) Move(_moveData[0].Keyword);
+        if(Input.GetKeyDown(KeyCode.D)) Move(_moveData[1].Keyword);
     }
 
     public void Move(string keyword)
@@ -49,5 +65,15 @@ public class PlayerController : MonoBehaviour
         notFound = true;
 
         return default;
+    }
+
+    public void AddLemur(LemurEntity lemurEntity)
+    {
+        if(!_lemurs.Contains(lemurEntity) && _formationsTemp.Length > _lemurs.Count)
+        {
+            _lemurs.Add(lemurEntity);
+            lemurEntity.transform.SetParent(_lemursParent);
+            lemurEntity.transform.localPosition = _formationsTemp[_lemurs.Count - 1];
+        }
     }
 }
