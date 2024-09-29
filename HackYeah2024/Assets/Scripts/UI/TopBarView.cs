3dbb1bb64e36fca543e416e2241bc733d4ec9bea
sgrabowski <sebastian.s.grabowski@gmail.com>
2024-09-29 10:31:08 +0200

    asdasd

--- HackYeah2024/Assets/Scripts/UI/TopBarView.cs
+++ HackYeah2024/Assets/Scripts/UI/TopBarView.cs
@@ -12,10 +12,13 @@ namespace Game.UI
         [SerializeField]private GameObject _view;
         [SerializeField]private TextMeshProUGUI _textMeshProUGUI;
         [SerializeField]private TextMeshProUGUI _textMeshProUGUIBg;
+        [SerializeField]private TextMeshProUGUI _btextMeshProUGUI;
+        [SerializeField]private TextMeshProUGUI _btextMeshProUGUIBg;
 
         public bool stop;
 
         string format = "Distance {0}m.";
+        string format2 = "BEST Distance {0}m.";
         float _distance;
         float _timeToElapse;
         
@@ -44,6 +47,17 @@ namespace Game.UI
                 
                 _textMeshProUGUI.text = string.Format(format, _distance.ToString("F2"));
                 _textMeshProUGUIBg.text = _textMeshProUGUI.text;
+                
+                var bestD = PlayerPrefs.GetFloat("BestD", 0.0f);
+                if(_distance > bestD)
+                {
+                    bestD = _distance;
+                    PlayerPrefs.SetFloat("BestD", _distance);
+                }
+                _btextMeshProUGUI.text = string.Format(format2, bestD.ToString("F2"));
+                _btextMeshProUGUIBg.text = _btextMeshProUGUI.text;
+                _btextMeshProUGUI.gameObject.SetActive(bestD > 0.0f);
+
                 _timeToElapse = Time.time + _delay;
             }
 
