7a08d7d2e0697e71c4d5f9a7a50f3cfcd05af9fb
Łukasz Surma <lukaszsurma73@gmail.com>
2024-09-29 07:40:48 +0200

    text effect setup

--- HackYeah2024/Assets/Scripts/UI/RightBarView.cs
+++ HackYeah2024/Assets/Scripts/UI/RightBarView.cs
@@ -8,8 +8,13 @@ namespace Game.UI
 
     public class RightBarView : MonoBehaviour
     {
+        [SerializeField] public float scaleMultiplier = 1.5f;
+        [SerializeField] private float duration = 0.5f;
         [SerializeField]private GameObject _view;
         [SerializeField]private TMP_Text _valueLabel;
+
+        string valueFormat = "x{0}";
+        int prevAmount = 0;
 
         public void Show()
         {
@@ -28,7 +33,38 @@ namespace Game.UI
         {
             if(PlayerController.Instance == null)
                 return;
-            _valueLabel.text = PlayerController.Instance.TeamCount.ToString();
+            
+            var teamCount = PlayerController.Instance.TeamCount;
+            if(teamCount > prevAmount) StartCoroutine(OnCollectEffect());
+
+            
+            _valueLabel.text = string.Format(valueFormat, teamCount);
+            prevAmount = teamCount;
         }
+
+        private IEnumerator OnCollectEffect()
+        {
+            Vector3 originalScale = _valueLabel.transform.localScale;
+            Vector3 targetScale = originalScale * scaleMultiplier;
+
+            float elapsedTime = 0f;
+            while (elapsedTime < duration)
+            {
+                _valueLabel.transform.localScale = Vector3.Lerp(originalScale, targetScale, elapsedTime / duration);
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+
+            elapsedTime = 0f;
+            while (elapsedTime < duration)
+            {
+                _valueLabel.transform.localScale = Vector3.Lerp(targetScale, originalScale, elapsedTime / duration);
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+
+            _valueLabel.transform.localScale = originalScale;
+        }
+
     }
 }
